1823c19a4cceb8ca51eab2fb9752f7f6f5d38dcd
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2016-10-11 10:20:49 +0200

    Add project files.

---
+++ Mocking/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mocking {
+    public class Employee {
+        public Employee() {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Wage { get; set; }
+
+        internal double CalculateWeeklySalary(int hours, int wage) {
+            return hours * wage;
+        }
+    }
+}
---
+++ Mocking/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mocking {
+    public class User {
+        public User() {
+        }
+        internal Rights Rights { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+        internal string ViewAllEmployees() {
+            return "Here is the list";
+        }
+    }
+}
